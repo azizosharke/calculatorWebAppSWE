1ade075a2656e3c486fa05e500464184c79917a9
taylorj8 <taylorj8@tcd.ie>
2022-10-23 18:02:09 +0100

    fixed bug with negation, added more detailed error messages

--- App.py
+++ App.py
@@ -1,3 +1,13 @@
 from functions import *
 
-print(f'Result: {calculator(input("Enter your Expression Here: ")):.9g}')
+
+if __name__ == "__main__":
+    running = True
+    while running:
+        result = calculator(input("Enter your Expression Here: "))
+        if result == "quit":
+            running = False
+        elif type(result) == str:
+            print(result)
+        else:
+            print(f'Result: {result}')
--- functions.py
+++ functions.py
@@ -2,15 +2,18 @@ from sys import exit
 
 pre = {')': 0, '(': 0, '+': 1, '-': 1, '*': 2, '/': 2, '^': 3}
 
+
 # Function to check if a given character is a number.
 # returns true/false.
 def is_number(token):
-    return str(token).replace('.', '').isdigit()
+    return str(token).replace('.', '').replace('-', '').isdigit()
+
 
 # Function to check if a given character is an operator.
 # returns true/false.
 def is_operator(token):
-    return token in ['+', '-', '*', '/', '^', '(', ')']
+    return token in ['+', '-', '*', '/', '^']
+
 
 # Function to check the operation and division by 0 .
 # returns the results .
@@ -25,97 +28,191 @@ def operation(num1, operator, num2):
         try:
             return num1 / num2
         except ZeroDivisionError:
-            print("DIVISION BY 0 ERROR.")
-            exit()
+            return "Error: division by zero"
     elif operator == "^":
         return num1 ** num2
+
 
 # Function to solve a mathematical equation
 # Supports (), +, -, *, /, ^, and floating point numbers.
 def calculator(equation: object) -> object:
-    list = list_conversion(equation)
-    for input in list:
-        if is_number(input) or is_operator(input):
-            continue
-        print('Invalid token: ' + input)
-        exit()
-    return floating_numbers(string_conversion(equation))  
+    list = convert_to_list(equation)
+    if type(list) == str:
+        return list
 
-# Function to solve the expression in a list . Takes a list form and outputs a floating point number.
-def floating_numbers(stringInput):
-    global m, n
-    list = []
-    for number in stringInput:
-        if not is_number(number):
-            try:
-                n = list.pop()
-                m = list.pop()
-            except IndexError:
-                print("Invalid expression ! ")
-                exit()
-            list.append(operation(m, number, n))
-        else:
-            list.append(number)
-    return list.pop()
+    validation_result = validateExpression(list)
+    if validation_result is not None:
+        return validation_result
+    else:
+        result = evaluate(list)
+        if type(result) == int or type(result) == float:
+            return round(result, 3)
+        return "Error: Complex Result"
 
-# Function that converts a string containing a mathematical expression into a list in Reverse Polish Notation
-
-def string_conversion(calc):
-    expression = list_conversion(calc)
-    result = []
-    list = []
-    for input in expression:
-        if not is_number(input):
-            if input == '(':
-                list.insert(0, input)
-            elif input == ')' and list:
-                while list[0] != '(' and list:
-                    result.append(list[0])
-                    del list[0]
-                if list and list[0] == '(':
-                    pass
-                else:
-                    print("Invalid formatting of parentheses in expression.")
-                    exit()
-                del list[0]
-            else:
-                if not list:
-                    pass
-                else:
-                    while (list and
-                           (input != '^' and pre.get(input) <= pre.get(list[0])) or
-                           (input == '^' and pre.get(input) < pre.get(list[0]))):
-                        result.append(list[0])
-                        del list[0]
-                list.insert(0, input)
-        else:
-            result.append(input)
-    while list:
-        result.append(list[0])
-        del list[0]
-    return result
 
 # Function to convert a string expression into a list
+def convert_to_list(input_to_calc: str):
+    last_num = False
+    # if true, next operator will be applied to number (for negation)
+    next_unary = True
+    negate = False
+    floating = 0    # if number floating (ie not 0), divide char by 10^floating and add to last index
+    expr = []
+    for char in input_to_calc:
+        if is_number(char):
+            if floating != 0:
+                if char == ".":
+                    return "Error: number has two decimal points"
+                if negate:
+                    # convert string to number, subtract from end of number in list
+                    expr[-1] = expr[-1] - ((ord(char) - 48) / 10 ** floating)
+                else:
+                    # convert string to number, add onto end of number in list
+                    expr[-1] = expr[-1] + ((ord(char) - 48) / 10 ** floating)
+                floating += 1
+            elif last_num:
+                if negate:
+                    # convert string to number, subtract from end of number in list
+                    expr[-1] = expr[-1] * 10 - (ord(char) - 48)
+                else:
+                    # convert string to number, add onto end of number in list
+                    expr[-1] = expr[-1] * 10 + (ord(char) - 48)
+            else:
+                last_num = True
+                if negate:
+                    expr.append((ord(char) - 48) * -1)
+                else:
+                    # convert from string to number and add to list
+                    expr.append(ord(char) - 48)
 
-def list_conversion(s):
-    empty_list = []
-    i = 0
-    while i != len(s):
-        if is_number(s[i]):
-            temp = s[i]
-            while True:
-                if i + 1 >= len(s):
-                    temp = float(temp)
-                    empty_list.append(temp)
-                    break
-                elif not (not is_number(s[i + 1]) and not (s[i + 1] == '.')):
-                    temp += s[i + 1]  # | |
-                    i += 1
-                    continue
-                temp = float(temp)
-                empty_list.append(temp)
-                break
-        elif s[i] != ' ':
-            empty_list.append(s[i])
-        i += 1
-    return empty_list
+            # if operator follows number, it is not unary
+            next_unary = False
+        elif char == " ":
+            last_num = False
+            floating = False
+        elif char == "-" and next_unary:
+            negate = not negate
+            floating = 0
+        elif char == ")":
+            expr.append(char)
+            last_num = False
+            negate = False
+            floating = 0
+        elif char == '.':
+            if last_num == "":
+                return "Error: must be a number before decimal point"
+            if floating != 0:
+                return "Error: number has two decimal points"
+            floating += 1
+        else:
+            expr.append(char)
+            last_num = False
+            negate = False
+            next_unary = True
+            floating = 0
+    print(expr)
+    return expr
+
+
+def validateExpression(expression):
+    if is_operator(expression[0]):
+        return "Error: starts with operator"
+    if is_operator(expression[-1]):
+        return "Error: ends with operator"
+
+    last_op = " "
+    last_num = ""
+    last_bracket = ""
+    brackets = 0
+    for i in expression:
+        if type(i) == int or type(i) == float:
+            if last_num != "":
+                return "Error: two numbers in a row: " + last_num + " and " + str(i)
+            if last_bracket == ")":
+                return "Error: operator needed after right bracket"
+            last_op = ""
+            last_num = str(i)
+            last_bracket = ""
+        elif is_operator(i):
+            if last_op != "":
+                return "Error: two operators in a row: " + last_op + " and " + i
+            if last_bracket == "(":
+                return "Error: operator after left bracket"
+            last_op = i
+            last_num = ""
+            last_bracket = ""
+        elif i == "(":
+            if last_op == "":
+                return "Error: operator needed before left bracket"
+            brackets += 1
+            last_op = ""
+            last_num = ""
+            last_bracket = "("
+        elif i == ")":
+            if last_op != "":
+                return "Error: operator before right bracket"
+            brackets -= 1
+            last_op = ""
+            last_num = ""
+            last_bracket = ")"
+        else:
+            return "Error: unrecognised character: " + str(i)
+
+    if brackets > 0:
+        return "Error: open left bracket"
+    if brackets < 0:
+        return "Error: open right bracket"
+    return None
+
+
+def getPrecedence(token):
+    if token == "+":
+        return 1
+    elif token == "-":
+        return 1
+    elif token == "*":
+        return 2
+    elif token == "/":
+        return 2
+    elif token == "^":
+        return 3
+    return -1
+
+
+def performOperation(val_stack, op_stack):
+    val2 = val_stack.pop()
+    val1 = val_stack.pop()
+    op = op_stack.pop()
+    res = operation(val1, op, val2)
+    if type(res) == str:
+        return res
+    val_stack.append(res)
+    return None
+
+
+def evaluate(expr):
+    val_stack = []
+    op_stack = []
+    for token in expr:
+        if type(token) == int or type(token) == float:
+            val_stack.append(token)
+        elif token == "(":
+            op_stack.append(token)
+        elif token == ")":
+            while len(op_stack) != 0 and op_stack[-1] != "(":
+                err = performOperation(val_stack, op_stack)
+                if err is not None:
+                    return err
+            op_stack.pop()  # discard "("
+        else:
+            while len(op_stack) != 0 and op_stack[-1] != "(" and getPrecedence(op_stack[-1]) >= getPrecedence(token):
+                err = performOperation(val_stack, op_stack)
+                if err is not None:
+                    return err
+            op_stack.append(token)
+
+    while len(op_stack) != 0:
+        err = performOperation(val_stack, op_stack)
+        if err is not None:
+            return err
+    return val_stack.pop()
