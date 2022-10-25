8839e85053515d8bbd7aae9eb988db9f2de0a235
Aziz <74883676+azizosharke@users.noreply.github.com>
2022-10-25 17:19:18 +0100

    Add files via upload

---
+++ frontend/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <calculator></calculator>
+  </div>
+</template>
+
+<script>
+import Calculator from './components/Calculator.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Calculator
+  }
+}
+</script>
+
+<style>
+  body {
+    background: #686868 !important;
+  }
+</style>
